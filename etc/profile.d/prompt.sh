4d7ff470cb9fadc602602b78e0a0b61dea309c2c
Markus Koller <markus-koller@gmx.ch>
2011-11-17 19:22:49 +0100

    Sync dotfiles

--- etc/profile.d/prompt.sh
+++ etc/profile.d/prompt.sh
@@ -10,8 +10,9 @@ if type __git_ps1 &>/dev/null; then
   export GIT_PS1_SHOWSTASHSTATE=1
   export GIT_PS1_SHOWUNTRACKEDFILES=1
 
+  export GIT_PS1='$(__git_ps1 "[\[[1;32m\]%s\[[0m\]] ")'
   export SUDO_PS1=$PS1
-  export PS1=$PS1'$(__git_ps1 "[\[[1;32m\]%s\[[0m\]] ")'
+  export PS1=$PS1$GIT_PS1
 fi
 
 # Set window titles when displaying prompt
