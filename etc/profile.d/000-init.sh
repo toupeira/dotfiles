6741ddc4f12580d3dd296719b0fd605a83ea0bd9
Markus Koller <markus-koller@gmx.ch>
2011-12-03 15:37:31 +0100

    Sync dotfiles

--- etc/profile.d/000-init.sh
+++ etc/profile.d/000-init.sh
@@ -8,7 +8,14 @@
 if [ -z "$SSH_AUTH_SOCK" ]; then
   # Load SSH agent if necessary
   exec ssh-agent -- bash --login
-elif [ -z "$STY" -a -n "$SSH_CONNECTION" -a -x /usr/bin/screen -a -f ~/.screenrc ]; then
+elif [ -z "$STY" -a -n "$SSH_CONNECTION" -a -x /usr/bin/screen -a -d ~/.byobu ]; then
   # Load Screen for SSH sessions
+
+  # Maintain link to SSH agent
+  if [ -S "$SSH_AUTH_SOCK" ] && [ ! -h "$SSH_AUTH_SOCK" ]; then
+    ln -sf "$SSH_AUTH_SOCK" ~/.byobu/.ssh-agent
+  fi
+  export SSH_AUTH_SOCK=~/.byobu/.ssh-agent
+
   exec screen -qRR
 fi
