882186ea50a7970539f94c61a8a5da71feb9e11b
Markus Koller <markus-koller@gmx.ch>
2011-12-03 21:46:04 +0100

    Sync dotfiles

--- etc/profile.d/environment.sh
+++ etc/profile.d/environment.sh
@@ -1,10 +1,5 @@
 export PATH=/usr/sbin:/usr/bin:/sbin:/bin:/usr/local/sbin:/usr/local/bin:/usr/games:/var/lib/gems/1.8/bin:~/bin
 
-export LANG="de_CH.UTF-8"
-export LC_MESSAGES="en_US.UTF-8"
-export LC_NUMERIC="en_US.UTF-8"
-export LC_CTYPE="en_US.UTF-8"
-
 export EDITOR="vim"
 export PAGER="less"
 
