ee5017efd7001127a9277862217458b2fb402ca2
Markus Koller <markus-koller@gmx.ch>
2012-01-08 23:53:04 +0100

    Sync dotfiles

--- etc/profile.d/z99-fnord.sh
+++ etc/profile.d/z99-fnord.sh
@@ -5,6 +5,7 @@ export LANG="de_CH.UTF-8"
 export LC_MESSAGES="en_US.UTF-8"
 export LC_NUMERIC="en_US.UTF-8"
 export LC_CTYPE="en_US.UTF-8"
+export GDM_LANG="$LANG"
 
 export VDPAU_NVIDIA_SYNC_DISPLAY_DEVICE="DFP-1"
 #export __GL_SYNC_TO_VBLANK="1"
