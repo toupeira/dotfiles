40f57927029f1d2720a7aabbebe739e89d598c0b
Markus Koller <markus-koller@gmx.ch>
2012-08-15 01:04:16 +0200

    Sync dotfiles

--- etc/profile.d/z10-fnord.sh
+++ etc/profile.d/z10-fnord.sh
@@ -1,9 +1,9 @@
 export RAILS_ENV=development
 
 export LANG="de_CH.UTF-8"
-export LC_MESSAGES="en_US.UTF-8"
-export LC_NUMERIC="en_US.UTF-8"
-export LC_CTYPE="en_US.UTF-8"
+export LC_MESSAGES="en_GB.UTF-8"
+export LC_NUMERIC="en_GB.UTF-8"
+export LC_CTYPE="en_GB.UTF-8"
 export GDM_LANG="$LANG"
 
 export VDPAU_NVIDIA_SYNC_DISPLAY_DEVICE="DFP-1"
