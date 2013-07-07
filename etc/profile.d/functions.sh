f858d012e5dd38f5a216a33cc4f111d59bd8d991
Markus Koller <markus-koller@gmx.ch>
2013-07-08 00:46:12 +0200

    Sync dotfiles

--- etc/profile.d/functions.sh
+++ etc/profile.d/functions.sh
@@ -140,12 +140,13 @@ function sw {
 
       pushd "$dir" >/dev/null
       local changes=`git status -s | grep -c .`
-      local has_changes=1
+      local other=`git status | grep -v "^# On branch .*" | fgrep -vx "nothing to commit, working directory clean"`
 
-      if [ $changes -gt 0 ]; then
+      if [ $changes -gt 0 -o -n "$other" ]; then
+        local has_changes=1
         echo
         echo -e " \e[1;32m>\e[1;33m $changes\e[1;37m changes in \e[1;36m[`realpath "$PWD"`]\e[0m" | sed -r "s|$HOME|~|"
-        git -c color.ui=always "$@" | sed -r 's/^/    /'
+        git -c color.ui=always status | sed -r 's/^/    /'
       fi
       popd >/dev/null
     done
