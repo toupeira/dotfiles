9172fc3cc319fa0aae1f6c21d992d912135386b7
Markus Koller <markus-koller@gmx.ch>
2013-07-09 16:54:48 +0200

    Sync dotfiles

--- etc/profile.d/functions.sh
+++ etc/profile.d/functions.sh
@@ -164,13 +164,13 @@ function src {
         if [ $changes -gt 0 ]; then
           local label="changes"
           [ $changes -eq 1 ] && label="change"
-          label="\e[1;33m$changes\e[1;37m $label"
+          label="$changes\e[1;37m $label"
         else
           label="Unmerged changes"
         fi
 
         [ -z "$first" ] && echo
-        echo -e " \e[1;32m>\e[1;37m $label in \e[1;36m[`realpath "$dir"`]\e[0m" | sed -r "s|$HOME|~|"
+        echo -e " \e[1;32m>\e[1;37m \e[1;33m$label in \e[1;36m[`realpath "$dir"`]\e[0m" | sed -r "s|$HOME|~|"
         (cd "$dir"; git -c color.ui=always status | sed -r 's/^/    /')
       else
         [ $last -gt 0 ] && echo
