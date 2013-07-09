d9dadc3f2e07a93d7242931dad6a27611a96f4e1
Markus Koller <markus-koller@gmx.ch>
2013-07-09 02:43:38 +0200

    Sync dotfiles

--- etc/profile.d/functions.sh
+++ etc/profile.d/functions.sh
@@ -141,7 +141,7 @@ function src {
       local unmerged=`cd "$dir"; git status | grep -v "^# On branch .*" | fgrep -vx "nothing to commit, working directory clean"`
 
       if [ $changes -gt 0 -o -n "$unmerged" ]; then
-        [ $changes -eq 0 ] && changes="\e[1;35mUnmerged"
+        [ $changes -eq 0 ] && changes="Unmerged"
         echo -e " \e[1;32m>\e[1;33m $changes\e[1;37m changes in \e[1;36m[`realpath "$dir"`]\e[0m" | sed -r "s|$HOME|~|"
         (cd "$dir"; git -c color.ui=always status | sed -r 's/^/    /')
       else
