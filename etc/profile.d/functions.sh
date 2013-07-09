6cf921f06212561ce2af3b9fd61505773ea5a4b8
Markus Koller <markus-koller@gmx.ch>
2013-07-09 02:41:16 +0200

    Sync dotfiles

--- etc/profile.d/functions.sh
+++ etc/profile.d/functions.sh
@@ -133,6 +133,7 @@ fi
 # Switch project directories and run Git commands in them
 function src {
   if [[ "$1" =~ ^(|st|status)$ ]]; then
+    echo
     for dir in ~/src/*; do
       [ -d "$dir/.git" ] || continue
 
@@ -140,15 +141,12 @@ function src {
       local other=`cd "$dir"; git status | grep -v "^# On branch .*" | fgrep -vx "nothing to commit, working directory clean"`
 
       if [ $changes -eq 0 -a -z "$other" ]; then
-        echo -e " \e[1;32m>\e[0m No changes in \e[0;36m[`realpath "$dir"`]\e[0m" | sed -r "s|$HOME|~|"
+        echo -e " \e[0;32m>\e[0m No changes in \e[0;36m[`realpath "$dir"`]\e[0m" | sed -r "s|$HOME|~|"
       else
-        echo
         echo -e " \e[1;32m>\e[1;33m $changes\e[1;37m changes in \e[1;36m[`realpath "$dir"`]\e[0m" | sed -r "s|$HOME|~|"
         (cd "$dir"; git -c color.ui=always status | sed -r 's/^/    /')
-        echo
       fi
     done
-
     echo
     return
   fi
