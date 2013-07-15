9326a92caea41d4a68d8e678ed2793d524fddb2b
Markus Koller <markus-koller@gmx.ch>
2013-07-16 01:21:29 +0200

    Sync dotfiles

--- etc/profile.d/prompt.sh
+++ etc/profile.d/prompt.sh
@@ -63,10 +63,10 @@ if type __git_ps1 &>/dev/null; then
 fi
 
 # Set window titles when displaying prompt
-if [[ "$TERM" =~ ^(rxvt|xterm-256color|screen-bce) ]]; then
-  if [ "$STY" ]; then
+if [[ "$TERM" =~ ^(rxvt|xterm-256color|screen-) ]]; then
+  if [ -n "$STY" ]; then
     export PROMPT_COMMAND='_pwd=${PWD/$HOME/\~}; echo -ne "\033]0;$HOSTNAME:$_pwd\007\033k$_pwd\033\\"'
-  elif [ "$SSH_CONNECTION" ]; then
+  elif [ -n "$SSH_CONNECTION" ]; then
     export PROMPT_COMMAND='_pwd=${PWD/$HOME/\~}; echo -ne "\033]1;$HOSTNAME:$_pwd\007\033]2;$USER@$HOSTNAME:$_pwd\007"'
   else
     export PROMPT_COMMAND='_pwd=${PWD/$HOME/\~}; echo -ne "\033]1;$_pwd\007\033]2;$_pwd\007"'
