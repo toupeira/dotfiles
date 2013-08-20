bf403fed1c8bcd8ef229374ea94536385079bad6
Markus Koller <markus-koller@gmx.ch>
2013-08-20 23:47:17 +0200

    Sync dotfiles

--- etc/profile.d/z99-ruby.sh
+++ etc/profile.d/z99-ruby.sh
@@ -1,37 +1,22 @@
 # Check for interactive bash
 [ -n "$BASH_INTERACTIVE" ] || return
 
-# Wrapper for RubyGems
-function gem_exec {
-  local version="$1"
-  local command="gem$version"
+# sudo wrapper for RubyGems
+function _gem_exec {
+  local command="$1"
   shift
 
-  which "$command" &>/dev/null || command="gem"
-
-  # use correct Gem path
-  if [ -d /var/lib/gems ]; then
-    local home="/var/lib/gems/$version/"
-  elif [ -d /opt/ruby-enterprise/lib/ruby/gems ]; then
-    local home="/opt/ruby-enterprise/lib/ruby/gems/$version/"
-  else
-    echo "Can't find Gem path on this system!"
-    return 1
-  fi
-
-  # use sudo for administrative commands
   if [[ "$1" =~ (install|uninstall|update|clean) ]]; then
-    command="sudo GEM_HOME=$home $command"
+    command="sudo $command"
   fi
 
-  GEM_HOME=$home $command "$@"
+  $command "$@"
 }
 
 # Aliases for different Ruby versions
-alias gem="gem_exec 1.8"
-alias gem1.8="gem_exec 1.8"
-alias gem1.9.1="gem_exec 1.9.1"
-alias gem2.0="gem_exec 2.0"
+alias gem="_gem_exec /usr/bin/gem"
+alias gem1.8="_gem_exec /usr/bin/gem1.8"
+alias gem1.9.1="_gem_exec /usr/bin/gem1.9.1"
 
 # Add wrappers for all bundler executables
 for command in `find ~/{src,www}/*/.bundle/ruby/*/bin -maxdepth 1 -type f -executable -printf "%f\n" 2>/dev/null | sort | uniq`; do
@@ -59,6 +44,11 @@ function bundle_exec {
     fi
   done
 
-  # pass the command to bundler
-  bundle exec "$command" "$@"
+  if ls $pwd/.bundle/ruby/*/bin/$command &>/dev/null; then
+    # run the command with bundler
+    bundle exec "$command" "$@"
+  else
+    # run the command from the system
+    command "$command" "$@"
+  fi
 }
