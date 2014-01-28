51a2f9336910f2b952bdf703c166c0d46573e466
Markus Koller <markus-koller@gmx.ch>
2014-01-28 14:19:48 +0100

    Sync dotfiles

--- etc/profile.d/001-environment.sh
+++ etc/profile.d/001-environment.sh
@@ -1,4 +1,9 @@
-export PATH="/usr/sbin:/usr/bin:/sbin:/bin:/usr/local/sbin:/usr/local/bin:/usr/games:/var/lib/gems/1.8/bin"
+export PATH="/usr/sbin:/usr/bin:/sbin:/bin:/usr/local/sbin:/usr/local/bin:/usr/games"
+
+if [ -d /var/lib/gems/1.8/bin ]; then
+  export PATH="$PATH:/var/lib/gems/1.8/bin"
+fi
+
 if [ -d ~/bin ]; then
   export PATH="$PATH":~/bin
 fi
@@ -8,12 +13,16 @@ if [ -x /usr/libexec/path_helper ]; then
   eval `/usr/libexec/path_helper -s`
 fi
 
-export EDITOR="sensible-vim"
+if has sensible-vim; then
+  export EDITOR="sensible-vim"
+else
+  export EDITOR="vim"
+fi
+
 export GIT_EDITOR="vim"
 export PAGER="less"
 
 export LESS="-iRM"
 export GREP_OPTIONS="-i --color=auto --exclude=.svn --exclude=.git --exclude=.*.swp"
-export ACK_OPTIONS="--smart-case --ignore-dir=tmp/cache --ignore-dir=tmp/files"
 export IRB_HISTORY_SIZE=3000
 export RI="--format ansi --no-pager"
