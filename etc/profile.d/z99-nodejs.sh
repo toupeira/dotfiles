cd299b0922c3170d3d44f776a3811dba40bf13b7
Markus Koller <markus-koller@gmx.ch>
2014-02-12 15:36:19 +0100

    Sync dotfiles

--- etc/profile.d/z99-nodejs.sh
+++ etc/profile.d/z99-nodejs.sh
@@ -1,6 +1,8 @@
 # Check for interactive bash
 [ -n "$BASH_INTERACTIVE" ] || return
 
+alias npm='sudo npm -g'
+
 # Add wrappers for all npm executables
 for command in `find ~/{src,www}/*/node_modules/*/bin -maxdepth 1 -type f -executable -printf "%f\n" 2>/dev/null | sort | uniq`; do
   eval "function $command { npm_exec \"$command\" \"\$@\"; }"
@@ -10,7 +12,7 @@ unset command
 function npm_exec {
   local pwd="$PWD"
   local command="$1"
-  local bin=`npm bin`/"$command"
+  local bin=`command npm bin`/"$command"
   shift
 
   if [ -x "$bin" ]; then
