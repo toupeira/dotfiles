3417bb4dc166fe294a6f887314afa8a13e02bf76
Markus Koller <markus-koller@gmx.ch>
2014-09-08 08:52:53 +0200

    Add nenv

--- etc/profile.d/nodejs.sh
+++ etc/profile.d/nodejs.sh
@@ -1,4 +1,10 @@
 # Check for interactive bash
 [ -n "$BASH_INTERACTIVE" ] || return
 
+# load nenv
+if [ -d ~/.nenv ]; then
+  export PATH="~/.nenv/bin:$PATH"
+  eval "$(nenv init - --no-rehash)"
+fi
+
 alias sunpm='sudo npm -g'
