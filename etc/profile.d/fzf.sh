93e30a2a84aa9bf69c797e56fdec3ee3b46572e0
Markus Koller <markus-koller@gmx.ch>
2015-03-19 22:07:53 +0100

    Sync dotfiles

--- etc/profile.d/fzf.sh
+++ etc/profile.d/fzf.sh
@@ -5,7 +5,7 @@ export FZF_DEFAULT_COMMAND='ag -l -g ""'
 export FZF_DEFAULT_OPTS='-x -m'
 export FZF_COMPLETION_TRIGGER='//'
 
-. "`dotfiles --path`/fzf/fzf-completion.bash"
+. "$( dotfiles --path )/fzf/shell/completion.bash"
 
 _fzf_orig_completion_ping='_ssh'
 _fzf_orig_completion_telnet='_ssh'
