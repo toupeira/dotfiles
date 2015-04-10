09556535c0c10cb36825fa4c6e16a5e66f814e0c
Markus Koller <markus-koller@gmx.ch>
2015-04-10 21:04:49 +0200

    Give $PATH priority to ~/bin

--- etc/profile.d/001-environment.sh
+++ etc/profile.d/001-environment.sh
@@ -2,7 +2,7 @@
 export LANG LC_MESSAGES LC_NUMERIC LC_CTYPE
 
 export PATH="/usr/sbin:/usr/bin:/sbin:/bin:/usr/local/sbin:/usr/local/bin:/usr/games"
-[ -d ~/bin ] && export PATH="$PATH:$HOME/bin"
+[ -d ~/bin ] && export PATH="$HOME/bin:$PATH"
 [ -x /usr/libexec/path_helper ] && eval `/usr/libexec/path_helper -s`
 
 export EDITOR="vim"
