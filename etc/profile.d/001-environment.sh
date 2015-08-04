351b65e06d6f9c97b10e9158482e82e6c15614da
Markus Koller <markus-koller@gmx.ch>
2015-08-04 19:23:25 +0200

    bash: Set PHANTOMJS_BIN if available

--- etc/profile.d/001-environment.sh
+++ etc/profile.d/001-environment.sh
@@ -23,3 +23,5 @@ export LESS_TERMCAP_us=$'\e[1;33m'
 export LESS_TERMCAP_me=$'\e[0m'
 export LESS_TERMCAP_se=$'\e[0m'
 export LESS_TERMCAP_ue=$'\e[0m'
+
+[ -x ~/bin/phantomjs ] && export PHANTOMJS_BIN=~/bin/phantomjs
