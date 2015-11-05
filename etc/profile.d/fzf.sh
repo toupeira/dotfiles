0299ba85c6a6acc06d208be4277ba88030605103
Markus Koller <markus-koller@gmx.ch>
2015-11-05 14:06:44 +0100

    bash: Enable host completion for curl

--- etc/profile.d/fzf.sh
+++ etc/profile.d/fzf.sh
@@ -12,6 +12,7 @@ _fzf_orig_completion_ping='_ssh'
 _fzf_orig_completion_telnet='_ssh'
 _fzf_orig_completion_host='_ssh'
 _fzf_orig_completion_nc='_ssh'
+_fzf_orig_completion_curl='_ssh'
 
-complete -F _fzf_complete_ssh ping telnet host nc
+complete -F _fzf_complete_ssh ping telnet host nc curl
 complete -o bashdefault -o default -F _fzf_path_completion ag bun
