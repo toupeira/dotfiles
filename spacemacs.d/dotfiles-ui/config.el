d76c018e3f4856946626c91d1ba503abbbc2eb53
Markus Koller <markus-koller@gmx.ch>
2016-06-10 15:36:17 +0200

    emacs: Tweak configuration

--- spacemacs.d/dotfiles-ui/config.el
+++ spacemacs.d/dotfiles-ui/config.el
@@ -2,10 +2,8 @@
 (setq linum-format "%5d")
 
 ;; set default size of new windows
-(unless dotfiles/is-ocelot
-  (add-to-list 'default-frame-alist '(width  . 120))
-  (add-to-list 'default-frame-alist '(height . 60))
-)
+(add-to-list 'default-frame-alist '(width  . 120))
+(add-to-list 'default-frame-alist '(height . 60))
 
 ;; customize theme
 (setq theming-modifications
