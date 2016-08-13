4b968d9222001645f03cf2e099a0395f892c1080
Markus Koller <markus-koller@gmx.ch>
2016-08-13 16:30:30 +0200

    emacs: Switch SPC q r and SPC q R keybindings

--- spacemacs.d/dotfiles-evil/keybindings.el
+++ spacemacs.d/dotfiles-evil/keybindings.el
@@ -5,22 +5,22 @@
   "RET" 'dotfiles/save-buffer
   "`"   'spacemacs/default-pop-shell
 
+  "qz" 'spacemacs/prompt-kill-emacs
+  "qZ" 'spacemacs/kill-emacs
+  "qr" 'spacemacs/restart-emacs
+  "qR" 'spacemacs/restart-emacs-resume-layouts
+
   "ws" 'split-window-below-and-focus
   "wS" 'split-window-below
   "wv" 'split-window-right-and-focus
   "wV" 'split-window-right)
 
 (which-key-add-key-based-replacements
-  "SPC S" "split-window-below"
-  "SPC V" "split-window-right")
+  "SPC -" "split-window-below"
+  "SPC |" "split-window-right")
 
 ;; use qq/qQ to keep server running
 (when dotspacemacs-persistent-server
   (spacemacs/set-leader-keys
     "qq" 'dotfiles/prompt-frame-killer
     "qQ" 'spacemacs/frame-killer))
-
-;; use qz/qZ to kill server
-(spacemacs/set-leader-keys
-  "qz" 'spacemacs/prompt-kill-emacs
-  "qZ" 'spacemacs/kill-emacs)
