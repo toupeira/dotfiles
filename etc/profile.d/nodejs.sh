128481cfe9eaf55b9cb8a78adf8ee467cb91913a
Markus Koller <markus-koller@gmx.ch>
2018-06-15 15:47:27 +0200

    nodejs: Use yarn for global packages

--- etc/profile.d/nodejs.sh
+++ etc/profile.d/nodejs.sh
@@ -1,10 +1,17 @@
 # Check for interactive bash
 [ -n "$BASH_INTERACTIVE" ] || return
 
+export NODE_PATH=/usr/lib/nodejs:/usr/lib/node_modules:/usr/share/javascript
+
 # load nenv
 if [ -d ~/.nenv ]; then
   export PATH="$HOME/.nenv/bin:$PATH"
   eval "$(nenv init - --no-rehash)"
+fi
+
+# add yarn global packages
+if [ -d ~/.yarn/bin ]; then
+  export PATH="$PATH:$HOME/.yarn/bin"
 fi
 
 alias sunpm='sudo npm -g'
