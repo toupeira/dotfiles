07e136b60b3a1df14f6f4d18ebf45da89f23d3e5
Markus Koller <markus-koller@gmx.ch>
2019-02-19 23:04:36 +0100

    Sync dotfiles

--- etc/profile.d/fzf.sh
+++ etc/profile.d/fzf.sh
@@ -2,11 +2,17 @@
 [ -n "$BASH_INTERACTIVE" ] && has fzf || return
 
 export FZF_TMUX=0
-export FZF_DEFAULT_COMMAND='fdfind --hidden --ignore-file ~/.ignore --type file --color always'
+export FZF_DEFAULT_COMMAND="fdfind --hidden --ignore-file $HOME/.ignore --color always --type f"
 export FZF_DEFAULT_OPTS='--ansi --multi --filepath-word --inline-info --reverse --prompt "» "'
 export FZF_COMPLETION_TRIGGER='//'
+export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND --type d --type l"
+export FZF_ALT_C_COMMAND="${FZF_DEFAULT_COMMAND/--type f/--type d}"
+
+_fzf_compgen_path() { $FZF_CTRL_T_COMMAND; }
+_fzf_compgen_dir() { $FZF_ALT_C_COMMAND; }
 
 . /usr/share/bash-completion/completions/fzf
+. /usr/share/doc/fzf/examples/key-bindings.bash
 
 _fzf_orig_completion_ping='_ssh'
 _fzf_orig_completion_telnet='_ssh'
