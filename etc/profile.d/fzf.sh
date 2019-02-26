f766cdede51de5c77bc121067f0150dbdcb55d3a
Markus Koller <markus-koller@gmx.ch>
2019-02-26 14:36:52 +0100

    Sync dotfiles

--- etc/profile.d/fzf.sh
+++ etc/profile.d/fzf.sh
@@ -3,10 +3,14 @@
 
 export FZF_TMUX=0
 export FZF_DEFAULT_COMMAND="fdfind --hidden --ignore-file $HOME/.ignore --color always --type f"
-export FZF_DEFAULT_OPTS='--ansi --multi --filepath-word --inline-info --reverse --height 10 --prompt "» "'
+export FZF_DEFAULT_OPTS="--ansi --multi --filepath-word --inline-info --layout default --no-height --prompt '» ' --bind '?:toggle-preview'"
 export FZF_COMPLETION_TRIGGER='//'
+
 export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND --type d --type l"
 export FZF_ALT_C_COMMAND="${FZF_DEFAULT_COMMAND/--type f/--type d}"
+
+export FZF_CTRL_R_OPTS="--preview 'echo {} | cut -d\  -f4-' --preview-window up:3:hidden:wrap"
+export FZF_ALT_C_OPTS="--preview 'tree -C {}' --preview-window hidden"
 
 _fzf_compgen_path() { $FZF_CTRL_T_COMMAND; }
 _fzf_compgen_dir() { $FZF_ALT_C_COMMAND; }
