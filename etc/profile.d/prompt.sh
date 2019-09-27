af53ed0986568a7281438864489e7448833125c6
Markus Koller <markus-koller@gmx.ch>
2019-09-27 18:53:50 +0200

    Sync dotfiles

--- etc/profile.d/prompt.sh
+++ etc/profile.d/prompt.sh
@@ -11,7 +11,7 @@ PS1_HOST=" "
 [ -n "$EMACS" ] && PS1_USER="" && PS1_HOST=""
 [ "$UID" = "0" ] && PS1_USER="\[\e[0m\e[1;31m\]$PS1_USER"
 
-PS1="\[\e[1;35m\]\$(_prompt_jobs)\[\e[0m\]\[\e[2m\]$PS1_USER\[\e[1;33m\]$PS1_HOST\[\e[0;36m\][\[\e[1;36m\]\$(_prompt_path)\[\e[0;36m\]]\[\e[0m\] "
+PS1="\[\e[1;35m\]\$(_prompt_jobs)\[\e[0m\]\[\e[1;30m\]$PS1_USER\[\e[1;33m\]$PS1_HOST\[\e[0;36m\][\[\e[1;36m\]\$(_prompt_path)\[\e[0;36m\]]\[\e[0m\] "
 PS2=" \[\e[1;35m\]»\[\e[0m\] "
 
 # Prompt helpers
