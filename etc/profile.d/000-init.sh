f715d976c28dcc837829b9b0045157cdb7117c61
Markus Koller <markus-koller@gmx.ch>
2020-01-18 21:14:42 +0100

    Sync dotfiles

--- etc/profile.d/000-init.sh
+++ etc/profile.d/000-init.sh
@@ -19,7 +19,16 @@ shopt -s globstar
 # Disable flow control (Ctrl-S/Q)
 stty -ixon
 
-if [ -z "$TMUX" ] && [ -n "$SSH_CONNECTION" ] && [ -x ~/bin/tmux-reattach ]; then
-  # Load tmux for SSH sessions if it's set up
-  exec ~/bin/tmux-reattach
+if [ -z "$TMUX" ] && type tmux &>/dev/null; then
+  # ignore stale tmux sockets
+  if [ -n "$TMUX" ] && [ ! -S "$TMUX" ]; then
+    unset TMUX
+  fi
+
+  session=$( tmux list-sessions 2>/dev/null | grep -vm1 attached | cut -d: -f1 )
+  if [ -n "$session" ] && [ $# -eq 0 ]; then
+    exec tmux attach-session -t "$session"
+  else
+    exec tmux new-session "$@"
+  fi
 fi
