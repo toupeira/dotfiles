639859f71b0eab47762eabb8655e39de198d0b98
Markus Koller <markus-koller@gmx.ch>
2021-02-11 17:22:12 +0100

    Sync dotfiles

--- etc/profile.d/ruby.sh
+++ etc/profile.d/ruby.sh
@@ -8,10 +8,6 @@ if [ -d ~/.asdf ]; then
   . ~/.asdf/asdf.sh
 fi
 
-# Aliases and functions
-alias be='bundle exec'
-alias rspec='bundle exec rspec -f doc'
-
 function gem {
   if [ "$1" = "cd" ]; then
     local path
@@ -27,16 +23,6 @@ function gem {
   command gem "$@"
 }
 
-# Rails/Rake wrapper
-function r {
-  if grep -q ' rails ([5-9]\.' Gemfile.lock &>/dev/null || [[ "$1" =~ ^(s|server|c|console|g|generate|d|destroy|r|runner|db|dbconsole|new)$ ]]; then
-    rails "$@"
-  else
-    bundle exec rake "$@"
-  fi
-}
-
-# Spring wrappers
 function _spring_exec {
   local command="$1"
   shift
@@ -48,6 +34,11 @@ function _spring_exec {
   fi
 }
 
+alias be='bundle exec'
+alias rspec='bundle exec rspec -f doc'
+alias rubocop='bundle exec rubocop'
+
+alias r='rails'
+alias sr='srails'
 alias srails='_spring_exec rails'
-alias srake='_spring_exec rake'
 alias srspec='_spring_exec rspec -f doc'
