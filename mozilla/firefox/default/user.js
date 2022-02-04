a8f00412381014eaf5894f7566062083ac9268a1
Markus Koller <markus-koller@gmx.ch>
2022-02-04 19:43:47 +0100

    firefox: Add user.js

---
+++ mozilla/firefox/default/user.js
@@ -0,0 +1,12 @@
+user_pref("ui.key.menuAccessKeyFocuses", false);
+user_pref("ui.key.textcontrol.prefer_native_key_bindings_over_builtin_shortcut_key_definitions", true);
+
+user_pref("browser.search.hiddenOneOffs", "Google,DuckDuckGo");
+user_pref("browser.search.separatePrivateDefault", true);
+user_pref("browser.search.separatePrivateDefault.ui.enabled", true);
+user_pref("browser.search.suggest.enabled", false);
+user_pref("browser.search.suggest.enabled.private", false);
+user_pref("browser.urlbar.suggest.calculator", true);
+user_pref("browser.urlbar.trimURLs", false);
+
+user_pref("extensions.pocket.enabled", false);
