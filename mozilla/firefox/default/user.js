a6cafc1131473896b843e9b1fd2dd3abbaf1ef68
Markus Koller <markus-koller@gmx.ch>
2022-03-23 01:18:39 +0100

    Sync dotfiles

--- mozilla/firefox/default/user.js
+++ mozilla/firefox/default/user.js
@@ -1,6 +1,4 @@
-user_pref("ui.key.menuAccessKeyFocuses", false);
-user_pref("ui.key.textcontrol.prefer_native_key_bindings_over_builtin_shortcut_key_definitions", true);
-
+user_pref("browser.download.alwaysOpenPanel", false);
 user_pref("browser.search.hiddenOneOffs", "Google,DuckDuckGo");
 user_pref("browser.search.separatePrivateDefault", true);
 user_pref("browser.search.separatePrivateDefault.ui.enabled", true);
@@ -8,5 +6,8 @@ user_pref("browser.search.suggest.enabled", false);
 user_pref("browser.search.suggest.enabled.private", false);
 user_pref("browser.urlbar.suggest.calculator", true);
 user_pref("browser.urlbar.trimURLs", false);
+
+user_pref("ui.key.menuAccessKeyFocuses", false);
+user_pref("ui.key.textcontrol.prefer_native_key_bindings_over_builtin_shortcut_key_definitions", true);
 
 user_pref("extensions.pocket.enabled", false);
