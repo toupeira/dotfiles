0471c30722377385e3eb9c8df65377e465949428
Markus Koller <markus@snafu.ch>
2024-11-12 18:31:34 +0100

    Sync dotfiles

--- vim/plugins/avante.lua
+++ vim/plugins/avante.lua
@@ -9,28 +9,12 @@ return {
     'nvim-lua/plenary.nvim',
     'MunifTanjim/nui.nvim',
     'echasnovski/mini.nvim',
-    {
-      'HakonHarnes/img-clip.nvim',
-      event = 'VeryLazy',
-      opts = {
-        default = {
-          embed_image_as_base64 = false,
-          prompt_for_file_name = false,
-          drag_and_drop = {
-            insert_mode = true,
-          },
-        },
-      },
-    },
-    {
-      'MeanderingProgrammer/render-markdown.nvim',
-      opts = {
-        file_types = { 'markdown', 'Avante' },
-      },
-      ft = { 'markdown', 'Avante' },
-      config = {
-        enabled = false,
-      }
-    },
+  },
+
+  opts = {
+    hints = { enabled = false },
+    behaviour = {
+      auto_apply_diff_after_generation = true,
+    }
   },
 }
