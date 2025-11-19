78a7185e26bc8f1d91c271da9685a6b50a06cdb8
Markus Koller <markus@snafu.ch>
2025-11-19 20:18:21 +0100

    Sync dotfiles

--- vim/plugins/themes.lua
+++ vim/plugins/themes.lua
@@ -2,7 +2,6 @@
 
 return {
   { 'catppuccin/nvim', name = 'catppuccin', lazy = true },
-  { 'folke/tokyonight.nvim', lazy = true },
   { 'marko-cerovac/material.nvim', lazy = true },
 
   { 'EdenEast/nightfox.nvim',
