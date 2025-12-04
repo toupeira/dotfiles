763153ae5f76a69486d3193fefd01ac38cfc5734
Markus Koller <markus@snafu.ch>
2025-12-04 11:45:26 +0100

    Sync dotfiles

--- ipython/profile_default/ipython_config.py
+++
@@ -1,9 +0,0 @@
-import sys
-
-c = get_config()
-
-c.InteractiveShellApp.exec_lines = ['import os, sys']
-
-c.TerminalInteractiveShell.banner1 = "\npython " + sys.version.replace("\n", "") + "\n"
-c.TerminalInteractiveShell.confirm_exit = False
-c.TerminalInteractiveShell.autocall = 1
