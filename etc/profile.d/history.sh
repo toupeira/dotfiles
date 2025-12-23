6e0a97e0ff7e681d293c8763155db8a56d6bf070
Markus Koller <markus@snafu.ch>
2025-12-23 15:41:24 +0100

    Sync dotfiles

--- etc/profile.d/history.sh
+++ etc/profile.d/history.sh
@@ -7,7 +7,7 @@ mkdir -p ~/.local/state/history
 [ "$HISTFILE" ] && HISTFILE=~/.local/state/history/bash
 HISTSIZE=50000
 HISTFILESIZE=50000
-HISTCONTROL="ignoredups"
+HISTCONTROL="erasedups"
 HISTTIMEFORMAT=$( echo -ne "\e[0;35m[%F \e[1;35m%T\e[0;35m]\e[0m " )
 
 export SQLITE_HISTORY=~/.local/state/history/sqlite
