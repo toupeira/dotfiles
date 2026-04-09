6729d1642043713667a8f5d2e5ebb3764bef74a7
Markus Koller <markus@snafu.ch>
2026-04-09 22:26:41 +0200

    Sync dotfiles

--- vim/plugins/filetypes.lua
+++ vim/plugins/filetypes.lua
@@ -3,7 +3,6 @@ local util = require('util')
 return {
   { 'antonk52/markdowny.nvim', ft = { 'markdown' }, config = true },
   { 'elixir-lang/vim-elixir',  ft = { 'elixir', 'eelixir' }},
-  { 'habamax/vim-godot',       ft = { 'gdscript', 'gsl' }},
   { 'hail2u/vim-css3-syntax',  ft = { 'css', 'scss' }},
   { 'hashivim/vim-terraform',  ft = { 'terraform', 'hcl' }},
 
